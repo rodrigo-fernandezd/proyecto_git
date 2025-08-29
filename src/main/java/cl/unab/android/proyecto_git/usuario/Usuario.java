53260a2f182bc3ce9a2e46f27deb9be557d9aeca
Seba <halbarad.mcleod@gmail.com>
2025-08-28 20:49:20 -0400

    Sebastián Añadí los constructores, los getters y setters

--- src/main/java/cl/unab/android/proyecto_git/usuario/Usuario.java
+++ src/main/java/cl/unab/android/proyecto_git/usuario/Usuario.java
@@ -1,15 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cl.unab.android.proyecto_git.usuario;
 
-/**
- *
- * @author seba
- */
 public class Usuario {
+
     private String nombre;
     private String mail;
-    
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nombre, String mail) {
+        this.nombre = nombre;
+        this.mail = mail;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
 }
