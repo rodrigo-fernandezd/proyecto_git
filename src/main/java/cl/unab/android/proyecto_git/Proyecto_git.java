a7653b2d6b7516cf0284338005ea754db40e1882
Rodrigo Fernandez <rodfernandezd@gmail.com>
2025-08-28 21:22:56 -0400

    Se inicia menu en el Main

--- src/main/java/cl/unab/android/proyecto_git/Proyecto_git.java
+++ src/main/java/cl/unab/android/proyecto_git/Proyecto_git.java
@@ -1,22 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package cl.unab.android.proyecto_git;
 
-/**
- *
- * @author seba
- */
 public class Proyecto_git {
 
     public static void main(String[] args) {
-        
+
+        Menu menu = new Menu();
+        menu.mostrarMenu();
+
         // Crear un menu
         // Crear opcion para crear usuario
         // Crear opcion para eliminar usuario
         // Crear opcion para salir
-        
-        
+
     }
+
 }
